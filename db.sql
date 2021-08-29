9aae7c8474436dcfb87256ad9c3ff9773574eeec
jess-small <jessica.small251@gmail.com>
2021-08-29 17:20:29 +1200

    Changed way references was declared and added drop table

--- db.sql
+++ db.sql
@@ -1,3 +1,6 @@
+drop table user;
+drop table budget;
+drop table budget_item;
 
 CREATE TABLE user
 (
@@ -11,10 +14,12 @@ CREATE TABLE user
 CREATE TABLE budget
 (
    budget_id    INT(9)          PRIMARY KEY,
-   user_id	INT(9)          NOT NULL REFERENCES user(uid)
-   budget_type  VARCHAR(15)
+   user_id	INT(9)          NOT NULL,
+   budget_type  VARCHAR(15),
    income	INT(6)          NOT NULL,
-   remaining    INT(6)
+   remaining    INT(6),
+   foreign key (uid)
+   references user(uid)
    
    
 );
@@ -22,7 +27,9 @@ CREATE TABLE budget
 CREATE TABLE budget_item
 (
    item_id	INT(9)          PRIMARY KEY,
-   category     VARCHAR(20)    
-   amount	INT             NOT NULL           
-   b_id         INT(9)          NOT NULL REFERENCES budget(budget_id)
+   category     VARCHAR(20),    
+   amount	INT             NOT NULL,           
+   b_id         INT(9)          NOT NULL,
+   foreign (budget_id)
+   references budget(budget_id)
 );
