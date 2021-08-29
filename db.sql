ae430754d5b172b7e51de977647cf609b452b7a5
jess-small <jessica.small251@gmail.com>
2021-08-29 17:43:18 +1200

    Changed syntax of foreign key references

--- db.sql
+++ db.sql
@@ -1,6 +1,3 @@
-drop table user;
-drop table budget;
-drop table budget_item;
 
 CREATE TABLE user
 (
@@ -18,8 +15,9 @@ CREATE TABLE budget
    budget_type  VARCHAR(15),
    income	INT(6)          NOT NULL,
    remaining    INT(6),
-   foreign key (uid)
-   references user(uid)
+   constraint fk_budget_uid
+   foreign key (user_id)
+   	references user(uid)
    
    
 );
@@ -30,6 +28,7 @@ CREATE TABLE budget_item
    category     VARCHAR(20),    
    amount	INT             NOT NULL,           
    b_id         INT(9)          NOT NULL,
-   foreign (budget_id)
-   references budget(budget_id)
+   constraint fk_budget_item_budget_id
+   foreign key(b_id)
+   	references budget(budget_id)
 );
