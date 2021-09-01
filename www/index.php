3cdafe7eb0b1ff063ee76e23bf12d495802cad7d
jess-small <jessica.small251@gmail.com>
2021-09-01 15:54:53 +1200

    updated index page to have login and sign up buttons

--- www/index.php
+++ www/index.php
@@ -1,30 +1,5 @@
-<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
-<html>
-<head><title>Database test page</title>
-<style>
-th { text-align: left; }
-
-table, th, td {
-  border: 2px solid grey;
-  border-collapse: collapse;
-}
-
-th, td {
-  padding: 0.2em;
-}
-</style>
-</head>
-
-<body>
-<h1>Database test page</h1>
-
-<p>Showing contents of papers table: ?</p>
-
-<table border="1">
-<tr><th>First Name</th><th>Last Name</th></tr>
-
 <?php
- 
+
 $db_host   = '192.168.2.12';
 $db_name   = 'fvision';
 $db_user   = 'webuser';
@@ -34,14 +9,23 @@ $pdo_dsn = "mysql:host=$db_host;dbname=$db_name";
 
 $pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
 
-$q = $pdo->query("SELECT * FROM user");
-
-while($row = $q->fetch()){
-  echo "<tr><td>".$row["fname"]."</td><td>".$row["lname"]."</td></tr>\n";
-}
-
 ?>
-</table>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body{ font: 14px sans-serif; text-align: center; }
+    </style>
+</head>
+<body>
+    <h1 class="my-5"></b>Budget Maker</h1>
+    <p>
+        <a href="/webpages/login.php" class="btn btn-warning">Login</a>
+        <a href="/webpages/signup.php" class="btn btn-danger ml-3">Sign Up</a>
+    </p>
 </body>
 </html>
-
