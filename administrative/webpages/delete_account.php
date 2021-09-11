e261742ba394d407a678d0d8c4468b4899f25bd5
jess-small <jessica.small251@gmail.com>
2021-09-11 13:58:56 +1200

    added SQL statements for deleting regular users and admins

---
+++ administrative/webpages/delete_account.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+$db_host   = '192.168.2.12';
+$db_name   = 'fvision';
+$db_user   = 'webuser';
+$db_passwd = 'insecure_db_pw';
+
+$pdo_dsn = "mysql:host=$db_host;dbname=$db_name";
+
+$pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
+
+
+if(isset($_GET['uid'])) {
+
+    $uid = $_GET['uid'];
+    $sql = "DELETE FROM administrator WHERE uid = :uid";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':uid', $uid, PDO::PARAM_INT);
+    $stmt->execute(); 
+    header("location: admin_home.php");
+    $stmt->close();
+    	
+}
+?>
